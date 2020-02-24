726278730183d778b41a32e6e66a84bb26f9cb29
apavlushina <ssemina7@gmail.com>
2020-02-24 12:35:45 +0100

    all info for post and date are rendered

--- src/components/main/PostsList.js
+++ src/components/main/PostsList.js
@@ -1,13 +1,29 @@
 import React from "react";
 export default function PostsList(props) {
+  const formatDate = date => {
+    let newDate = [];
+    newDate = [
+      "0" + date.getDate(),
+      "0" + (date.getMonth() + 1),
+      "" + date.getFullYear()
+    ].map(component => component.slice(-2));
+
+    return newDate.join(".");
+  };
+
   return (
     <div>
-      <h1>All Posts</h1>
-      <p>There are {props.posts.length} posts available.</p>
-
       <ul>
         {props.posts.map(post => {
-          return <li key={post.id}>{post.title}</li>;
+          return (
+            <li key={post.id}>
+              <img src={post.img_url} alt={post.title}></img>
+              <p>{post.category && post.category.name}</p>
+              <p>{formatDate(new Date(post.created_at))}</p>
+              <h2>{post.title}</h2>
+              <p>{post.content}</p>
+            </li>
+          );
         })}
       </ul>
     </div>
